fc029c9db6a147b110eab93ae314841a725bb64b
Norb Beaver <norb.beaver@gmail.com>
2014-01-23 10:50:25 +0200

    copyrights

--- com.digiarea.jse/src/com/digiarea/jse/utils/Enclosure.java
+++ com.digiarea.jse/src/com/digiarea/jse/utils/Enclosure.java
@@ -1,8 +1,13 @@
-/**
- * Copyright (c) 2009-2011 DigiArea, Inc. All rights reserved.
- * 
- * @author norb <norb.beaver@digi-area.com>
- */
+/*******************************************************************************
+ * Copyright (c) 2011 - 2014 DigiArea, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     DigiArea, Inc. - initial API and implementation
+ *******************************************************************************/
 package com.digiarea.jse.utils;
 
 import com.digiarea.jse.NameExpr;
